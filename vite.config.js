554efec43fef44d750f5ce8c7c36dc70a100834f
Megha KL <megha@nortvis.com>
2024-07-05 15:43:35 +0530

    Update project files

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Replace 'your-repo-name' with your actual repository name
+const base = '/social-links/';
 // https://vitejs.dev/config/
 export default defineConfig({
+  base,
   plugins: [react()],
 })
